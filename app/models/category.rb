0a246f411d2d2f71a96541cea21d267886e0175b
Ros <Fedorovch>
2022-11-14 17:04:04 +0200

    Create Doctor's category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   has_many :doctors
+
+  validates :category_name, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+c1 = Category.new category_name: "Physical Therapist"
+c1.save
+
+c2 = Category.new category_name: "Cardiologist"
+c2.save
+
+c3 = Category.new category_name: "Ophtalmologist"
+c3.save
+
+c4 = Category.new category_name: "Obstetrics and Gynecology"
+c4.save
