5f40ac164099f41573e14e48178d774904f9d334
Ros <Fedorovch>
2022-11-14 17:06:08 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :doctors
   devise_for :users
+
+  get '/doctors', to: 'doctors#index'
+
+  resources :categories
+  resources :appoitments
   root "main_pages#index"
 end
